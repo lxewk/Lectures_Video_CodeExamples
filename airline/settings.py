cc5dfc6bc93e00bee53e79a3d1694927656f4c27
Elles <44805356+lxewk@users.noreply.github.com>
2021-05-15 19:50:55 +0200

    Update settings.py

--- airline/settings.py
+++ airline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+$_$vt!qft^l33lsd5h2)(4_v%x%(d2s7#5)0!%sh=fn4ngu2i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
